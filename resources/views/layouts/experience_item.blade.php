08240cb495c888f87019a8051d8a3b5e0dfc5848
Nick DuBois <njdubois@gmail.com>
2018-09-16 17:14:37 -0500

    added work history

---
+++ resources/views/layouts/experience_item.blade.php
@@ -0,0 +1,26 @@
+<div class="experienceContainer">
+
+    <div class="experienceHeader">
+        <h1>Company Name</h1>
+        <h2>Company Location</h2>
+    </div>
+
+    <p class="experienceDates">Employment Date</p>
+    <p class="experienceDescription">
+        Job Description, blah.  My work here. Blip.  This is what I did at this position.  Blip
+        Blah. Job Description, blah.  My work here. Blip.  This is what I did at this
+        position.  Blip Blah. Job Description, blah.  My work here. Blip.  This is what I
+        did at this position.  Blip Blah. Job Description, blah.  My work here. Blip.  This is what
+        I did at this position.  Blip Blah.
+    </p>
+
+    <p class="experienceLabel">Utilized Skills:</p>
+    <ul>
+        <li>project</li>
+    </ul>
+
+    <p class="experienceLabel">Noted Projects:</p>
+    <ul>
+        <li>project</li>
+    </ul>
+</div>
